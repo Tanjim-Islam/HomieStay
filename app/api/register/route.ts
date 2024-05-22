f353d1acfca986812dd3661e02ee9f999ee23358
Tanjim Riju <tanjim.islam.riju@g.bracu.ac.bd>
2024-05-22 22:14:15 +0600

    deployment error fix

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,26 +1,24 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import prisma from "@/app/libs/prismadb";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(
-    request: Request
-) {
-    const body = await request.json();
-    const {
-        email,
-        name,
-        password
-    } = body;
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, name, password } = body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+  if (!email || !name || !password) {
+    return NextResponse.error();
+  }
 
-    const user = await prisma.user.create({
-        data: {
-            email,
-            name,
-            hashedPassword
-        }
-    });
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-    return NextResponse.json(user);
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }
